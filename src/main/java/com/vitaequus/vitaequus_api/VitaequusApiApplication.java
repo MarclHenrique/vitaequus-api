3949476cb055aee9f252090cf2113fa66d6dc6c3
MarclHenrique <mh593417@gmail.com>
2026-03-28 11:12:22 -0300

    Estrutura inicial projeto

---
+++ src/main/java/com/vitaequus/vitaequus_api/VitaequusApiApplication.java
@@ -0,0 +1,13 @@
+package com.vitaequus.vitaequus_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaequusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitaequusApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitaequus/vitaequus_api/VitaequusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitaequus.vitaequus_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaequusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
